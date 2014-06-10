cf8bbb41580b318bab9a98292d37b1d1839f3ecd
unknown <JPAcyber@Latihan45.jpa.gov.my>
2014-06-10 15:48:14 +0800

    Bilik Latihan ICT2

---
+++ form4.php
@@ -0,0 +1,20 @@
+<?php
+	$nama =$_POST["nama"];
+	$dob =$_POST["dob"];
+	$jantina =$_POST["jantina"];
+	$membaca =$_POST["membaca"];
+	$menulis =$_POST["menulis"];
+	$mengira =$_POST["mengira"];
+	$jenis_pekerjaan =$_POST["jp"];
+	$komen =$_POST["textarea"];
+	
+	echo "Nama :$nama <br />
+		Tarikh Lahir :$dob <br />
+		Jantina :$jantina <br />
+		Membaca :$membaca <br />
+		Menulis :$menulis <br />
+		Mengira :$mengira <br />
+		Jenis Pekerjaan :$jenis_pekerjaan <br />
+		komen :$komen ";
+
+?>
